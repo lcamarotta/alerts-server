2f1e1cc03d2808f416e14d36128e8441ced9f75a
Lucas Camarotta <lucas_camarotta@hotmail.com>
2023-03-07 11:40:24 -0300

    testing native player

--- src/app.js
+++ src/app.js
@@ -9,13 +9,15 @@ app.use(express.static("./public"));
 
 
 app.get('/', (req, res) => {
-  const player = require('play-sound')(opts = {})
-  try {
-    player.play('./assets/warning.wav', function(err){
-      if(err) throw new Error(err)
-    })
-  } catch (error) {
-    console.error("test 02", error)
-  }
-  res.status(200).send("Test Sound")
+  // const player = require('play-sound')(opts = {})
+  // try {
+  //   player.play('./assets/homer.wav', function(err){
+  //     if(err) throw new Error(err)
+  //   })
+  // } catch (err) {
+  //   console.error("ERROR", err)
+  // }
+  // res.status(200).send("Test Sound")
+  
+  res.status(200).send('./index.html')
 })
