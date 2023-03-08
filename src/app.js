384583fcb4f3bc522b5c18a401a0314b01719da7
Lucas Camarotta <lucas_camarotta@hotmail.com>
2023-03-08 17:21:12 -0300

    websocket works

--- src/app.js
+++ src/app.js
@@ -8,14 +8,20 @@ app.use(express.static(__path('src/public/')));
 const httpServer = app.listen(80, () => console.log('Server listening on port 80'));
 
 const io = new Server(httpServer)
+let countClients = 0;
+let countAlerts = 0;
 io.on('connection', socket => {
-  console.log('New client')
+  countClients ++
+  console.log(countClients, 'New client')
 });
 
-app.get('/', (req, res) => {
-  io.emit('alert', 'Alert received');
+app.get('/', (req, res) => {  
+  res.status(200).sendFile(__path('src/public/index.html'))
 })
 
-app.get('/client', (req, res) => {  
-  res.status(200).sendFile(__path('src/public/index.html'))
+app.get('/api/alert', (req, res) => {
+  countAlerts ++
+  console.log(countAlerts, 'Alerts');
+  io.emit('alert', `Alert ${countAlerts} received`);
+  res.status(200).send('')
 })
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,10 +1,12 @@
 const socket = io();
-socket.on('alert', (msg) => {
-  console.log(msg)
-})
-
 const soundPlayer = document.getElementById('soundPlayer')
 const testButton = document.getElementById('testButton')
+
 testButton.onclick = () => {
   soundPlayer.play();
 }
+
+socket.on('alert', (msg) => {
+  console.log(msg)
+  soundPlayer.play();
+})
