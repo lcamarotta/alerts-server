88cb4f18e25b2699bc966107b01ec012d2cfd577
Lucas Camarotta <lucas_camarotta@hotmail.com>
2023-03-09 14:09:23 -0300

    change audio files, log ip addresses

--- src/app.js
+++ src/app.js
@@ -5,15 +5,23 @@ import { __path } from './utils.js';
 const app = express();
 app.use(express.static(__path('src/public/')));
 
-const httpServer = app.listen(80, () => console.log('Server listening on port 80'));
-
+const httpServer = app.listen(8080, () => console.log('Server listening on port 8080'));
 const io = new Server(httpServer)
+
 let countClients = 0;
 let countAlerts = 0;
 io.on('connection', socket => {
   countClients ++
-  console.log(countClients, 'New client')
+  const address = socket.handshake.address;
+  console.log(`New client from ${address}.\n${countClients} online\n`)
+
+  socket.on('disconnect', (reason) => {
+    countClients --
+    const address = socket.handshake.address;
+    console.log(`Client ${address} disconnected.\n${countClients} online.\nReason: ${reason}\n`)
+  })
 });
+
 
 app.get('/', (req, res) => {  
   res.status(200).sendFile(__path('src/public/index.html'))
@@ -21,7 +29,7 @@ app.get('/', (req, res) => {
 
 app.get('/api/alert', (req, res) => {
   countAlerts ++
-  console.log(countAlerts, 'Alerts');
+  console.log(countAlerts, 'Alerts since last boot ');
   io.emit('alert', `Alert ${countAlerts} received`);
-  res.status(200).send('')
+  res.status(200).send({status: 'success'})
 })
